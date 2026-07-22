8aa8acfccebd0da2639fcc0306bcfaaef8da6b1e
suvarnagarikapudi143-ux <suvarnagarikapudi143@gmail.com>
2026-07-22 10:14:26 +0530

    Add files via upload

---
+++ primenumber.py
@@ -0,0 +1,9 @@
+n=int(input("Enter a Number:"))
+count = 0
+for i in range(1, n + 1):
+    if n % i == 0:
+        count += 1
+if count == 2:
+    print("Prime Number")
+else:
+    print("Not a Prime Number")
